745e819ed73fb132aba9aa6bb0ead17893cfbf48
santu-sarker <mdsantusarker@gmail.com>
2022-02-02 00:28:58 +0600

    changing footer description

--- side_views/footer.php
+++ side_views/footer.php
@@ -1,51 +1,46 @@
 <!-- Site footer -->
 <footer class="site-footer">
-      <div class="container justify-content-space-between">
-        <div class="row ">
-          <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-            <h6 >About</h6>
-            <p class="text-justify">Project <i>Product Review </i> is a Demonstration website for reviewing various product & facilities . This is the final project for group(Best Web Project) Batch: 10A11A . For More Details  Contact with  Us.Contact Details are Given in <i>Contact Us</i> Section.</p>
-          </div>
+      <!-- Site footer -->
+<div class="container-fluid justify-content-space-between">
+    <div class="row ">
+        <div class="col-lg-5 col-md-4 col-sm-12 col-xs-12">
+            <h6>About</h6>
 
-          <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 ">
-            <h6 class="team-header">Team Member</h6>
-            <ul class="footer-links ">
-              <li><a href="">Md Santu Sarker(Full Stack Dev)</a></li>
-              <li><a href="">Mehedi Hasan(Full Stack Dev)</a></li>
-              <li><a href="">Shimul Azad(Front-end Dev)</a></li>
-              <li><a href="">Sabrin Dia(Front-end Dev)</a></li>
-            </ul>
-          </div>
+            <p class="text-justify">Project <i>Product Review </i> is a Demonstration website for
+            reviewing various product & services where End user can share their Feedback.
+             More Details please Contact with  Us.</p>
 
-          <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-            <div class="contact-address btm-brdr">
-            <h6 class="section-header" > Contact <span>Us</span> </h6>
-            <ul class="footer-links ">
-                <li> <i class="fa fa-phone" aria-hidden="true"></i> <span>01766464051</span> </li>
-                <li> <i class="fa fa-map-marker" aria-hidden="true"></i> <span>Green-Road,Panthapath,Dhaka</span> </li>
-                <li><i class="fas fa-envelope" aria-hidden="true"></i><span> webproject11@gmail.com</span></li>
-            </ul>
-            </div>
-          </div>
         </div>
-        <hr>
-      </div>
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8 col-sm-6 col-xs-12">
-            <p class="copyright-text">Copyright &copy; 2021 All Rights Reserved by
-         <a href="#">Project_Product_Review_Team</a>.
-            </p>
-          </div>
 
-          <div class="col-md-4 col-sm-6 col-xs-12">
-            <ul class="social-icons">
+        <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12">
+            <div class="contact-address btm-brdr ml-2">
+                <h6 class="section-header"> Contact <span>Us</span> </h6>
+                <ul class="footer-links ">
+                    <li> <i class="bi bi-phone" aria-hidden="true"></i> <span>01766464051</span> </li>
+                    <li><i class="bi bi-envelope-open-fill" aria-hidden="true"></i><span> webproject11@gmail.com</span>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12 ml-2">
+            <h6 class="section-header text-center"> Social Links </h6>
+            <ul class="social-icons text-center">
               <li><a class="facebook" href="#"><i class="fab fa-facebook fa-lg"></i></a></li>
               <li><a class="twitter" href="#"><i class="fab fa-twitter fa-lg"></i></a></li>
               <li><a class="github" href="#"><i class="fab fa-github fa-lg"></i></a></li>
               <li><a class="linkedin" href="#"><i class="fab fa-linkedin fa-lg"></i></a></li>
             </ul>
-          </div>
         </div>
-      </div>
+    </div>
+    <hr>
+</div>
+<div class="container">
+    <div class="row col-12 justify-content-center">
+        <h6 class="copyright-text text-center ml-3">Copyright &copy; 2021 All Rights Reserved By
+            Md Santu Sarker.
+        </h6>
+    </div>
+</div>
+
 </footer>
--- side_views/index_navbar.php
+++ side_views/index_navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar fixed-top navbar-expand-sm navbar-dark bg-dark ">
     <div class="container-fluid">
-    <a href="./index.php" class="navbar-brand ">Contact Store</a>
+    <a href="./index.php" class="navbar-brand ">Review Web</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
